630b7ff7bb0bde34044e7c07b563943e2ede7e80
Gonzalo Valenzuela <gonzavalenzuela23@gmail.com>
2022-04-27 16:47:16 -0300

    Eliminando p3ejercicio8.py

--- p3ejercicio8.py
+++
@@ -1,9 +0,0 @@
-'''
-Un ISBN-10 (International Standard Book Number) consiste de 10 dígitos:
-d1d2d3d4d5d6d7d8d9d10.
-Si el dígito verificador es 10, el último dígito es x, de acuerdo a las normas ISBN. Escribir
-un programa que permita ingresar los primeros 9 dígitos como una cadena y muestre el
-número ISBN.
-Ejemplo: 013601267 ---> 0136012671
-         013031997 ---> 013601267X
-'''
